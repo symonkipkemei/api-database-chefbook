8cff2457010a1b6aef56d908181bcc228bd44f50
Caden MacKenzie <cadenmackenzie@gmail.com>
2019-04-22 08:42:33 -0600

    additional api and db labs

---
+++ apis/Exercise_07.py
@@ -0,0 +1,17 @@
+'''
+
+Create an application that interfaces with the user via the CLI - prompt the user with a menu such as:
+
+Please select from the following options (enter the number of the action you'd like to take):
+1) Create a new account (POST)
+2) View all your tasks (GET)
+3) View your completed tasks (GET)
+4) View only your incomplete tasks (GET)
+5) Create a new task (POST)
+6) Update an existing task (PATCH/PUT)
+7) Delete a task (DELETE)
+
+It is your responsibility to build out the application to handle all menu options above.
+
+
+'''
---
+++ databases/Exercise_04.py
@@ -0,0 +1,8 @@
+'''
+
+Please create a new Python application that interfaces with a brand new database.
+This application must demonstrate the ability to create at least 3 tables, insert to each table,
+update each table, select from each table, delete from each table, and use at least one join query.
+
+
+'''
