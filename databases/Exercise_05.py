5acf8e5437717f31a5e0be0de7ca7e1f3cb44ab2
Caden MacKenzie <cadenmackenzie@gmail.com>
2019-04-24 22:39:31 -0600

    using tasks api for database modeling

--- databases/Exercise_05.py
+++ databases/Exercise_05.py
@@ -1,31 +1,12 @@
 '''
-Create a Slack API token for the codingnomads workspace.
+Using the API from the API section, write a program that makes a request to
+get all of the users and all of their tasks.
 
-Using the slackclient package or the requests package, fetch all comments that include links
-from the python-resources channel.
+Create tables in a new local database to model this data.
 
-Create a list of dictionaries with the following format:
-[
-    {
-        "link": "the fetched URL",
-        "description": "short blurb describing the resource (if available)",
-        "date_added": "when was it posted?",
-        "read": False  # defaults to False, change to True if you read it
-        "rating": 0  # on a scale from 1-10, initially 0
-        "comments": [
-            "a list of strings",
-            "with comments on the resource",
-        ]
-        "starred": False,  # defaults to False, change to True if you think it's great
-    },
-    # next link item
-]
+Think about what tables are required to model this data. Do you need two tables? Three?
 
-Next, create the necessary tables in a database to model this data.
-
-Think about what tables are required to model this object. Do you need two tables? Three?
-
-Now, persist the data that you retrieve using the Slack API to your database.
+Persist the data returned from the API to your database.
 
 NOTE: If you run this several times you will be saving the same information in the table.
 To prevent this, you should add a check to see if the record already exists before inserting it.
