473471afa836865b4ef23635287b69481937cda5
Martin Breuss <martin-martin@users.noreply.github.com>
2019-07-23 09:16:38 +0930

    generalize

--- databases/Exercise_01.py
+++ databases/Exercise_01.py
@@ -2,6 +2,6 @@
 
 All of the following exercises should be done using sqlalchemy.
 
-Using the dvdrental schema, write the necessary code to print information about the film and category table.
+Using the provided database schema, write the necessary code to print information about the film and category table.
 
 '''
