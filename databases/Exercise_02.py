b7693ec4bd713ae860be9b1fece3d830ff0bfe49
Martin Breuss <martin-martin@users.noreply.github.com>
2019-10-15 15:41:57 +1030

    Fix spelling and clarify instructions

--- databases/Exercise_02.py
+++ databases/Exercise_02.py
@@ -1,16 +1,16 @@
 '''
-Using sqlalchemy which the necessary code to:
+Consider each of the tasks below as a separate database query. Using SQLAlchemy, which is the necessary code to:
 
 - Select all the actors with the first name of your choice
 
 - Select all the actors and the films they have been in
 
-- Select all the actors that have appeared in a category of you choice comedy
+- Select all the actors that have appeared in a category of a comedy of your choice
 
-- Select all the comedic films and that and sort them by rental rate
+- Select all the comedic films and sort them by rental rate
 
-- Using one of the statements above, add a GROUP BY
+- Using one of the statements above, add a GROUP BY statement of your choice
 
-- Using on of the statements above, add a ORDER BY
+- Using one of the statements above, add a ORDER BY statement of your choice
 
 '''
